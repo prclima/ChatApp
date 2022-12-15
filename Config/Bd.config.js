cfd627e9c5910fb2692ee8369e35e2446456d775
prclima <pedro@futuranet.com.br>
2022-12-15 14:28:44 -0300

    p

--- Config/Bd.config.js
+++ Config/Bd.config.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-
-    async function dbConnect(){
-    try{
-        const dbConnection = await mongoose.connect(process.env.BD_PORT, { useNewUrlParser: true })
-        console.log("conectado ao banco")
-    } catch(err){
-        console.log(err)
-    }
+async function dbConnect() {
+  try {
+    const dbConnection = await mongoose.connect(process.env.BD_PORT, {
+      useNewUrlParser: true,
+    });
+    console.log("conectado ao banco");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-module.exports = {dbConnect};
+module.exports = { dbConnect };
--- index.js
+++ index.js
@@ -1,70 +1,55 @@
-const express = require('express');
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
 
-const cors = require('cors')
-const { dbConnect }  = require("./Config/Bd.config.js")
-const userRoutes = require("./Router/userRoutes.js")
-const chatRoutes = require("./Router/chatRoutes.js")
-const messageRoutes = require("./Router/messageRoutes.js")
+const cors = require("cors");
+const { dbConnect } = require("./Config/Bd.config.js");
+const userRoutes = require("./Router/userRoutes.js");
+const chatRoutes = require("./Router/chatRoutes.js");
+const messageRoutes = require("./Router/messageRoutes.js");
 const createServer = require("http");
-const { use } = require('./Router/userRoutes.js');
-const path = require('path')
+const { use } = require("./Router/userRoutes.js");
+const path = require("path");
 
-dotenv.config()
-dbConnect()
+dotenv.config();
+dbConnect();
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
-app.use("/api/user", userRoutes)
-app.use("/api/chat", chatRoutes)
-app.use("/api/message", messageRoutes)
+const Server = app.listen(
+  Number(process.env.PORT),
+  console.log("Servidor no Ar")
+);
 
-// //--------------Deploy---------------------
-
-// const __dirname1 = path.resolve()
-
-// if(process.env.NODE_ENV === 'production'){
-//     app.user(exress.static(path.join(__dirname, "")))
-
-
-//     app.get('*', (req, res)=> {
-//         res.sendFile("Back is running")
-//     })
-// }
-
-
-
-const Server = app.listen(process.env.PORT,
-    console.log("Servidor no Ar"))
-
-const io = require('socket.io')(Server, {
-    pingTimeout: 60000,
-    cors: {       
-        methods: ["GET", "POST"]
-      }
-})
+const io = require("socket.io")(Server, {
+  pingTimeout: 60000,
+  cors: {
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log("Connected to socket.io")
-     socket.on("setup", (userData)=>{
-        socket.join(userData.data._id);
-        socket.emit("connected");
-     })
-    socket.on('join chat', (room) =>{
-        socket.join(room);
-        console.log('Usuario entrou na sala' + room)
-    })
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData.data._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("Usuario entrou na sala" + room);
+  });
 
-    socket.on('new message', (newMessageRecieved) => {
-        let chat = newMessageRecieved.chat;
+  socket.on("new message", (newMessageRecieved) => {
+    let chat = newMessageRecieved.chat;
 
-        chat.users.forEach((user) =>{
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
 
-            if (user._id == newMessageRecieved.sender._id) return;
-            
-            socket.in(user._id).emit("message recieved", newMessageRecieved)
-        });
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
-    });
+  });
+});
